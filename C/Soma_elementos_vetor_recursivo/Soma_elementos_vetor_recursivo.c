859ef101f0c7ec92288711629aed7f9f72d3b21a
laisjmvet <laisjmvet@gmail.com>
2022-11-29 21:48:49 -0300

    numeros perfeitos e soma de elementos de um vetor recursivo

---
+++ C/Soma_elementos_vetor_recursivo/Soma_elementos_vetor_recursivo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Crie uma função recursiva que retorne a soma dos elementos de um vetor de inteiros.
+
+int somaElementos (int qtdElementos, int *vetor) {
+    int soma = 0;
+
+    if (qtdElementos == 0) {
+        return soma;
+    }else {
+        soma = vetor[qtdElementos - 1] + soma;
+        return somaElementos(qtdElementos - 1, vetor) + soma;
+    }
+}
+
+int main()
+{
+  int vetor[] = {1, 2, 3, 10, 32};
+  int tam = sizeof(vetor)/sizeof(int);
+  int soma = somaElementos(tam, vetor);
+  printf("Soma: %d\n", soma);
+
+}
